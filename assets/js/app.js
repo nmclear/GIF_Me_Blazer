f668a1d88e8083bd75775a1bc96be923fae0a843
Nick Clear <nickclear22@gmail.com>
2018-05-19 17:04:24 -0700

    added button creator. fixed dynamic click listener issue

--- assets/js/app.js
+++ assets/js/app.js
@@ -5,7 +5,6 @@ $(document).ready(function() {
 // VARIABLES
 //========================================================================================================================
     
-    
     var topics = [
         "The Office",
         "Parks n Rec",
@@ -15,17 +14,17 @@ $(document).ready(function() {
         "Modern Family"
     ];
 
-
 //========================================================================================================================
 // FUNCTIONS
 //========================================================================================================================
-
 
     function createButton(){
         for(var i = 0; i < topics.length; i++){
             var button = $("<button>").text(topics[i]);
             button.attr('data-name', topics[i]);
+            button.addClass('gifButton');
             $('.buttonDisplay').append(button);
+            console.log(button.attr('data-name'));
         }
     }
 
@@ -36,17 +35,29 @@ createButton();
 //========================================================================================================================
 // AJAX FUNCTION
 //========================================================================================================================
-    // Button event listener
-    $("button").on("click", function() {
+    // Event listener to display new buttons
+    $("#searchSubmit").on("click", function(){
+        var text = $('#searchInput').val();
+        topics.push(text);
+        $('.buttonDisplay').empty();
+        createButton();
+    });
+
+
+
+
+// Button event listener to display GIFs
+    $('body').on("click", 'button.gifButton', function() {
         // store data-name from button clicked
+        $('.gifDisplay').empty();
+
+
         var topic = $(this).attr("data-name");
-  
+        console.log(topic);
         // Creating URL
         var queryURL = "https://api.giphy.com/v1/gifs/search?q=" +
           topic + "&api_key=1S5pFMN37AVDaiKL2Tf1dLIue0cXXVYB&limit=10";
   
-
-
         // AJAX call
         $.ajax({
           url: queryURL,
@@ -55,13 +66,11 @@ createButton();
         .then(function(response) {
             var results = response.data;
 
-
             for(var i = 0; i < results.length; i++) {
                 var gifDiv = $("<div class='item'>");
 
                 var gifRating = results[i].rating;
                 var pRating = $("<p>").text("Rating: " + gifRating);
-
                 var topicGIF = $("<img>");
 
                 topicGIF.attr("src", results[i].images.fixed_height.url);
@@ -70,14 +79,7 @@ createButton();
                 gifDiv.append(topicGIF);
 
                 $(".gifDisplay").prepend(gifDiv);
-
-
             }
-
-
-
-
-
           });
     });
 
